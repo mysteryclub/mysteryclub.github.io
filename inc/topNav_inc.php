5877997ea74cbe651036fc8b3e661f7db33f755d
webdeeziner <brrytucker@googlemail.com>
2021-07-27 15:48:17 +0100

    menu nav bar

--- inc/topNav_inc.php
+++ inc/topNav_inc.php
@@ -1,37 +1,40 @@
+<!-- nav bar start -->
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+  
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="index.html">HOME </a></li>
+          <li><a href="shop.html">SHOP</a></li>
+          <li><a href="pages.html">SEE PAGES</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT MYSTERY CLUB <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="aboutAuthor.html">About The Author</a></li>
+              <li><a href="aboutStory.html">About The Story</a></li>
+              <li><a href="aboutCharacters.html">About The Characters</a></li>
+             
+            </ul>
+            
+            <li><a href="news.html">NEWS &amp; REVIEWS</a></li>
+            
+            <li><a href="gallery.html">GALLERY</a></li>
+          <li><a href="http://blog.mysteryclub.co.uk/">BLOG</a></li>
+        </ul>
+        
+        
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="index.php">HOME </a></li>
-        <li><a href="shop.php">SHOP</a></li>
-        <li><a href="pages.php">SEE PAGES</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT MYSTERY CLUB <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="aboutAuthor.php">About The Author</a></li>
-            <li><a href="aboutStory.php">About The Story</a></li>
-            <li><a href="aboutCharacters.php">About The Characters</a></li>
-           
-          </ul>
-          
-          <li><a href="news.php">NEWS &amp; REVIEWS</a></li>
-          
-          <li><a href="gallery.php">GALLERY</a></li>
-        <li><a href="http://blog.mysteryclub.co.uk/">BLOG</a></li>
-      </ul>
-      
-      
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+  <!-- navbar end -->
